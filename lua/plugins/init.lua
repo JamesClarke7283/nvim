a437d2501b60fde1be937d4e73232fb1794eb598
James Clarke <james@jamesdavidclarke.com>
2024-04-14 12:17:58 +0100

    Added LSPs and Trouble diagnostics

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,33 +6,46 @@ return {
       require "configs.conform"
     end,
   },
+{
+        'folke/trouble.nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons',
+      lazy=false,  
+      config = function()
+            require("trouble").setup({
+                height = 30,
+            })
 
+            vim.keymap.set("n", "<leader>x", function()
+                vim.cmd("TroubleToggle")
+            end, { silent = true, noremap = true, desc = "Toggle Diagnostics Panel" })
+        end
+    },
   -- These are some examples, uncomment them if you want to see them work!
---   {
---     "neovim/nvim-lspconfig",
---     config = function()
---       require("nvchad.configs.lspconfig").defaults()
---       require "configs.lspconfig"
---     end,
---   },
---  
---   {
---   	"williamboman/mason.nvim",
---   	opts = {
---   		ensure_installed = {
---   			"lua-language-server", "stylua",
---   			"html-lsp", "css-lsp" , "prettier", "rust_analyzer"
---   		},
---   	},
---   },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+   {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+   },
+
+   {
+   	"williamboman/mason.nvim",
+   	opts = {
+   		ensure_installed = {
+   			"lua-language-server", "stylua",
+   			"html-lsp", "css-lsp" , "prettier", "rust-analyzer", "pyright", "deno","tailwindcss-language-server"
+   		},
+   	}
+   },
+
+   {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css"
+   		},
+   	},
+   },
 }
