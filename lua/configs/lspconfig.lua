bdf1f7e4c7b500c706bf368b49ce6bda7b46ef0f
James Clarke <james@jamesdavidclarke.com>
2024-04-15 16:16:07 +0100

    Fixed rust and tailwind lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "rust-analyzer", "pyright", "denols", "tailwindcss-language-server"}
+local servers = { "html", "cssls", "rust_analyzer", "pyright", "denols", "tailwindcss"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,49 +3,66 @@ return {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     config = function()
-      require "configs.conform"
+      require("configs.conform")
     end,
   },
-{
-        'folke/trouble.nvim',
-        dependencies = 'nvim-tree/nvim-web-devicons',
-      lazy=false,  
-      config = function()
-            require("trouble").setup({
-                height = 30,
-            })
 
-            vim.keymap.set("n", "<leader>x", function()
-                vim.cmd("TroubleToggle")
-            end, { silent = true, noremap = true, desc = "Toggle Diagnostics Panel" })
-        end
-    },
+  {
+    'folke/trouble.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    lazy = false,
+    config = function()
+      require("trouble").setup({
+        height = 30,
+      })
+
+      vim.keymap.set("n", "<leader>x", function()
+        vim.cmd("TroubleToggle")
+      end, { silent = true, noremap = true, desc = "Toggle Diagnostics Panel" })
+    end
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
-   {
-     "neovim/nvim-lspconfig",
-     config = function()
-       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-     end,
-   },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require("configs.lspconfig")
+    end,
+  },
 
-   {
-   	"williamboman/mason.nvim",
-   	opts = {
-   		ensure_installed = {
-   			"lua-language-server", "stylua",
-   			"html-lsp", "css-lsp" , "prettier", "rust-analyzer", "pyright", "deno","tailwindcss-language-server"
-   		},
-   	}
-   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp" , "prettier", "rust-analyzer", "pyright", "deno","tailwindcss-language-server"
+      },
+    }
+  },
 
-   {
-   	"nvim-treesitter/nvim-treesitter",
-   	opts = {
-   		ensure_installed = {
-   			"vim", "lua", "vimdoc",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
         "html", "css"
-   		},
-   	},
-   },
+      },
+    },
+  },
+  -- Ollama Copilot
+  {
+    "David-Kunz/gen.nvim",
+    lazy = false,
+    opts = {
+      model = "starling-lm:7b-beta-q6_K", -- The default model to use.
+      host = "ollama.jamesdavidclarke.com", -- The host running the Ollama service.
+      port = 443,
+      debug = false,
+      command = function(options)
+        local body = {model = options.model, stream = true}
+        return "curl --silent --no-buffer -X POST https://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+      end,
+    },
+  },
 }
