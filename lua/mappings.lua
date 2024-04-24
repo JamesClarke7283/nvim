8c6d613ce58873efebb4ea0eda019abd9baa39da
James Clarke <james@jamesdavidclarke.com>
2024-04-24 07:52:06 +0100

    Added Fugitive keybinds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,11 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Fugitive mappings
+map("n", "<leader>gs", ":Git status<CR>", { desc = "Git Status" })
+map("n", "<leader>ga", ":Git add", { desc = "Git Add" })
+map("n", "<leader>gc", ":Git commit", { desc = "Git Commit" })
+map("n", "<leader>gps", ":Git push<CR>", { desc = "Git Push" })
+map("n", "<leader>gpl", ":Git pull<CR>", { desc = "Git Pull" })
+map("n", "<leader>gl", ":Git log<CR>", { desc = "Git Log" })
